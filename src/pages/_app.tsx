6919638f7f4977b3f443ed0154b5b65b0cf80c93
kutarkutakuta <kutarkutakuta@gmail.com>
2023-09-10 00:19:23 +0900

    Buid Error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
-import MyLayout from "../components/MyLayout";
+import "@/styles/globals.scss";
+import MyLayout from "@/components/MyLayout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
