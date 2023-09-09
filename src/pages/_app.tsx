1700a4175976975310cfc0de305c2127134a6b0d
kutarkutakuta <kutarkutakuta@gmail.com>
2023-09-10 00:34:05 +0900

    エラー対応

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,8 @@
 import type { AppProps } from "next/app";
 import "@/styles/globals.scss";
-import MyLayout from "@/components/MyLayout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <MyLayout>
-      <Component {...pageProps} />
-    </MyLayout>
+    <Component {...pageProps} />
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import { Inter } from 'next/font/google'
 import MyComponent from '@/components/MyContent'
+import MyLayout from '@/components/MyLayout'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
+    <MyLayout>
       <MyComponent></MyComponent>
-    </>
+    </MyLayout>
   )
 }
