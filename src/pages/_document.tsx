ab643248b0a5fece90d79021b08be7592f7f9425
kutarkutakuta <kutarkutakuta@gmail.com>
2023-09-12 13:09:07 +0900

    manifest更新

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,6 +14,21 @@ const MyDocument = () => (
       />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       
+      {/* <!-- Facebook:カード用--> */}
+      <meta property="og:url" content="https://renkei-navi.onrender.com" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="FF11連携Navi" />
+      <meta property="og:description" content="FF11の連携を検索ナビゲート。直感的な操作で強い組み合わせが一目瞭然。PTメンバーに最適な連携が見つかります。" />
+      <meta property="og:image" content="https://renkei-navi.onrender.com/assets/icons/icon-128x128.png" />
+      {/* <!-- twitter:カード用--> */}
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="@kutakutar_ff11" />
+      <meta name="twitter:domain" content="renkei-navi.onrender.com" />
+      <meta name="twitter:url" content="https://renkei-navi.onrender.com" />
+      <meta name="twitter:title" content="FF11連携Navi" />
+      <meta name="twitter:description" content="FF11の連携を検索ナビゲート。直感的な操作で強い組み合わせが一目瞭然。PTメンバーに最適な連携が見つかります。" />
+      <meta name="twitter:image" content="https://renkei-navi.onrender.com/assets/icons/icon-128x128.png" />
+
       <meta
         name="google-site-verification"
         content="eKWsAU6fruzFLUVXCaNJ1VF4-8tcVAZrHDUYUNmsGOE"
@@ -28,7 +43,7 @@ const MyDocument = () => (
       <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&amp;display=swap" rel="stylesheet" />
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
       <link rel="manifest" href="manifest.webmanifest" />
-      <meta name="theme-color" content="#1976d2" />
+      <meta name="theme-color" content="#2d2d2d" />
     </Head>
     <body>
       <Main />
