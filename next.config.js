07a789e928c966ad087fc0231cd987e558fcdaba
A8\yuman <yuma.nagashima@gmail.com>
2025-12-06 11:17:23 +0900

    PWA開発時には無効に

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
+const isProd = process.env.NODE_ENV === 'production';
 const withPWA = require('next-pwa')({
-  dest: 'public'
+  dest: 'public',
+  // disable PWA in development to avoid GenerateSW being called multiple times
+  disable: !isProd,
+  // helpful defaults
+  register: true,
+  skipWaiting: true,
 })
 
 module.exports = withPWA({
